4cef21643b5d999e6df440b98f7e9b3e6c730f3a
Amanda Chang <achang140@my.bcit.ca>
2024-03-25 10:18:48 -0700

    Commit

--- audit_log/app.py
+++ audit_log/app.py
@@ -1,23 +1,44 @@
 import connexion
 from connexion import FlaskApp
 from flask_cors import CORS, cross_origin
-# from connexion.middleware import MiddlewarePosition
-# from starlette.middleware.cors import CORSMiddleware
 
+import os 
 import yaml 
 import json 
 import logging
 import logging.config
 from pykafka import KafkaClient
 
-with open('app_conf.yml', 'r') as f: 
+# with open('app_conf.yml', 'r') as f: 
+#     app_config = yaml.safe_load(f.read())
+
+# with open('log_conf.yml', 'r') as f: 
+#     log_config = yaml.safe_load(f.read()) 
+#     logging.config.dictConfig(log_config)
+
+# logger = logging.getLogger('basicLogger')
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f: 
-    log_config = yaml.safe_load(f.read()) 
-    logging.config.dictConfig(log_config)
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
 
+logging.config.dictConfig(log_config)
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
 
 def get_hotel_room(index):
     """ Get Hotel Room reservations in History """
